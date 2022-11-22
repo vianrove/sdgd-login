8ec3bd6b2104e8977de1c19b8f7a34ed7e48e55f
Jhon78 <jrobayo@utb.edu.co>
2022-11-22 10:11:39 -0500

    Añadiendo nueva ruta Get

--- routes/rutas.js
+++ routes/rutas.js
@@ -1,10 +1,19 @@
 import express from 'express';
-
+import { read } from '../db/mysql.js'
 import { viewAll, createClient, updateClient, deleteUser, login } from '../db/methods.js'
 
 const routes = express.Router();
 
 routes.get('/view',viewAll);
+
+routes.get('/listUsers',(req,res)=>{
+    read((result)=>{
+        if(result.length == 0){
+            res.status(500).json({"status":"error interno"})
+        }
+        res.json(result)
+    })
+});
 
 routes.post('/login',login);
 
